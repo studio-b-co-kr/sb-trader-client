4054bd92da446d7eaa51e6438891e934fedce137
YC <yakov@studio-b.co.kr>
2025-09-19 23:32:32 +0900

    add id and link

--- src/pages/TradesPage.tsx
+++ src/pages/TradesPage.tsx
@@ -6,7 +6,7 @@ import { useNetworkVariable } from '../configs/networkConfig'
 export default function TradesPage() {
   const transactionBookId = useNetworkVariable('transactionBookId')
   const [transactions, setTransactions] = useState<
-    { sender: string; side: string; price: string; quantity: string; symbol: string; exchange: string; filled_at: string; timestamp: string }[]
+    { id: unknown; sender: string; side: string; price: string; quantity: string; symbol: string; exchange: string; filled_at: string; timestamp: string }[]
   >([])
 
   const { data, isPending, error } = useSuiClientQuery(
@@ -27,6 +27,7 @@ export default function TradesPage() {
       const fields = content.fields as {
         transactions: {
           fields: {
+            id: unknown
             sender: string
             price: string
             side: string
@@ -51,6 +52,10 @@ export default function TradesPage() {
     const ms = n > 1e12 ? n : n * 1000;
     return dayjs(ms).format('YYYY-MM-DD HH:mm');
   }
+
+  function shortenAddress(id: string, length = 4) {
+    return `${id.slice(0, 2 + length)}...${id.slice(-length)}`;
+  }
   // TODO: add campaign id and trader id columns, add sorting, pagination
   return (
     <div className="container mx-auto my-4 px-4">
@@ -71,6 +76,7 @@ export default function TradesPage() {
             <table className="min-w-full bg-black-100 rounded ">
               <thead>
                 <tr>
+                  <th className="px-4 py-2 border-b border-pink-600 text-left font-semibold">ID</th>
                   <th className="px-4 py-2 border-b border-pink-600 text-left font-semibold">Side</th>
                   <th className="px-4 py-2 border-b border-pink-600 text-right font-semibold">Price</th>
                   <th className="px-4 py-2 border-b border-pink-600 text-right font-semibold">Quantity</th>
@@ -86,6 +92,22 @@ export default function TradesPage() {
                   const tdClass = `px-4 py-2 text-sm border-b border-pink-600${isLastRow ? ' border-b-0' : ''}`;
                   return (
                     <tr key={i} className="even:bg-gray-800">
+                      <td className={`${tdClass} font-mono text-right`}>
+                        <a href="https://testnet.suivision.xyz/object/0xc12298a1291d0d70df3c22aa70171bb1ca1531270d404a2b78263096d59a1e95"
+                          className="underline underline-offset-2">
+                          <div className='flex items-center justify-end gap-2'>
+                          <span>{shortenAddress((tx as { id: { id: string } }).id.id)}</span>
+                          {/* <button
+                            onClick={() => {
+                              navigator.clipboard.writeText((tx as { id: { id: string } }).id.id);
+                            }}
+                            className='text-xs text-blue-500 hover:underline'
+                          >
+                            copy
+                          </button> */}
+
+                        </div></a>
+                      </td>
                       <td className={tdClass}>{tx.side}</td>
                       <td className={`${tdClass} font-mono text-right`}>{tx.price}</td>
                       <td className={`${tdClass} font-mono text-right`}>{tx.quantity}</td>
